d3d8dd30550bd3fee44a71f19c162fb6aabf201e
Kamrul Hasan <kamrulsdev@gmail.com>
2022-06-18 05:49:28 +0600

    navbar and cat card added

---
+++ components/Card/Card.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './Card.module.css'
+
+export default function Card({ name, phone, email, image, id }) {
+    return (
+        <div className={styles.card}>
+            <div className={styles["card-header"]}>
+                <image src="" alt={image.alt} className={styles["card-img"]} />
+            </div>
+            <div>
+                <h3>{name}</h3>
+                <p>{phone}</p>
+                <p>{email}</p>
+            </div>
+        </div>
+    )
+}
---
+++ components/Jumbotron/Jumbotron.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Jumbotron() {
+    return (
+        <div className="jumbotron jumbotron-fluid mt-5">
+            <div className="container">
+                <h1 className="display-4">Cats World</h1>
+                <p className="lead">Find your best friend today. Adopt a Wonderful Cat</p>
+                <button type="button" className="btn btn-primary">Start Looking</button>
+            </div>
+        </div>
+    )
+}
